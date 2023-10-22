b830a3c3077d8a6d0f91c20694e1d8d05f8765d1
tomrcc <tom@cloudcannon.com>
2023-10-22 16:21:04 +1300

    Trying to fix cloudcannon postbuild

--- src/rosey/create-locale-file.js
+++ src/rosey/create-locale-file.js
@@ -1,60 +1,66 @@
 const fs = require('fs');
+const mv = require('mv');
 
 // read theme color from _data/site.json
-fs.readFile('./src/rosey/base.json', 'utf8', function (err, dataFile) {
+fs.readFile('./rosey/base.json', 'utf8', function (err, dataFile) {
   if (err) {
     console.log(err);
     return;
   }
 
-  fs.readFile(
-    './src/rosey/locales/ko-kr.json',
-    'utf8',
-    function (err, localeFile) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      localeFile = JSON.parse(localeFile);
-      dataFile = JSON.parse(dataFile);
-      const localeKeys = Object.keys(localeFile);
-      const baseKeys = Object.keys(dataFile.keys);
-      console.log(baseKeys);
-
-      const result = {};
-      for (let i = 0; i < baseKeys.length; i++) {
-        const key = baseKeys[i];
-        let localeValue = '';
-        if (localeFile[key]) {
-          localeValue = localeFile[key].value;
-        }
-        const original = dataFile.keys[key].original;
-        if (localeValue || localeValue === '') {
-          Object.assign(result, {
-            [key]: {
-              original: original,
-              value: localeValue,
-            },
-          });
-        }
-      }
-      const replacedFile = JSON.stringify(result);
-
-      // Write result
-      fs.writeFile(
-        './src/rosey/locales/ko-kr.json',
-        replacedFile,
-        'utf-8',
-        function (err) {
-          if (err) {
-            console.log(err);
-          }
-          console.log(
-            `📚 Writing variables to './src/rosey/locales/ko-kr.json'`
-          );
-        }
-      );
+  fs.readFile('./rosey/locales/ko-kr.json', 'utf8', function (err, localeFile) {
+    if (err) {
+      console.log(err);
+      return;
     }
-  );
+
+    localeFile = JSON.parse(localeFile);
+    dataFile = JSON.parse(dataFile);
+    const localeKeys = Object.keys(localeFile);
+    const baseKeys = Object.keys(dataFile.keys);
+    console.log(baseKeys);
+
+    const result = {};
+    for (let i = 0; i < baseKeys.length; i++) {
+      const key = baseKeys[i];
+      let localeValue = '';
+      if (localeFile[key]) {
+        localeValue = localeFile[key].value;
+      }
+      const original = dataFile.keys[key].original;
+      if (localeValue || localeValue === '') {
+        Object.assign(result, {
+          [key]: {
+            original: original,
+            value: localeValue,
+          },
+        });
+      }
+    }
+    const replacedFile = JSON.stringify(result);
+
+    // Write result
+    fs.writeFile(
+      './rosey/locales/ko-kr.json',
+      replacedFile,
+      'utf-8',
+      function (err) {
+        if (err) {
+          console.log(err);
+        }
+        console.log(`📚 Writing variables to './rosey/locales/ko-kr.json'`);
+
+        const currentPath = './rosey';
+        const destinationPath = './src/rosey';
+
+        mv(currentPath, destinationPath, function (err) {
+          if (err) {
+            throw err;
+          } else {
+            console.log('Successfully moved the file!');
+          }
+        });
+      }
+    );
+  });
 });
