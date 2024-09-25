0fc68bea8d45ac8b094ee91d0aa38678023fdae4
Alysha Nolan <alysha@cloudcannon.com>
2024-09-26 08:54:52 +1200

    Feat: add footer

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ const pluginBookshop = require("@bookshop/eleventy-bookshop");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css")
   eleventyConfig.addPassthroughCopy("src/assets/images")
+  eleventyConfig.addPassthroughCopy("node_modules/@fortawesome/fontawesome-free/css/all.min.css")
+  eleventyConfig.addPassthroughCopy("node_modules/@fortawesome/fontawesome-free/webfonts")
 
   eleventyConfig.addWatchTarget("component-library/");
 
