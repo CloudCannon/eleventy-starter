3fc03a5cedbf222ab316f4dfe31c4417add570f6
Alysha Nolan <alysha@cloudcannon.com>
2024-09-26 12:04:43 +1200

    Feat: add tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("node_modules/@fortawesome/fontawesome-free/css/all.min.css")
   eleventyConfig.addPassthroughCopy("node_modules/@fortawesome/fontawesome-free/webfonts")
 
+  eleventyConfig.addWatchTarget('tailwind.config.js');
+	eleventyConfig.addWatchTarget('src/assets/styles/tailwind.css');
+  eleventyConfig.addWatchTarget('src/assets/styles/compiled-tailwind.css');
   eleventyConfig.addWatchTarget("component-library/");
 
   eleventyConfig.addPlugin(pluginBookshop({
