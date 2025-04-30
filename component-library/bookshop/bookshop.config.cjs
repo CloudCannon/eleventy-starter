d797bcc2da58f40d35b273a8b52a077c1c02c6ff
tomrcc <tom@cloudcannon.com>
2025-05-01 11:13:13 +1200

    Test image fix and renderContent replaceAll change

--- component-library/bookshop/bookshop.config.cjs
+++ component-library/bookshop/bookshop.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   engines: {
     "@bookshop/eleventy-engine": {
-      plugins: ["./image.js"],
+      plugins: ["./image.js", "./renderContent.js"],
     },
   },
 };
--- component-library/bookshop/renderContent.js
+++ component-library/bookshop/renderContent.js
@@ -7,7 +7,7 @@ export default function (Liquid) {
       /{% bookshop 'snippets\/alert' background_color: "(.*?)" alert_message: "(.*?)" color: "(.*?)" %}/gim;
 
     const alertShortcodeRendered =
-      /<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: $1; color: $3;"><p class="!mb-0"> $2 \<\/p\>\<\/div\>/;
+      '<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: $1; color: $3;"><p class="!mb-0"> $2 </p></div>';
 
     const valueWithRenderedShortcode = value.replaceAll(
       alertShortcodeRegex,
