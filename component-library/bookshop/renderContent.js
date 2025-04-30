5683cf4b203f2deea928348e007ad1c07b0d61fd
tomrcc <tom@cloudcannon.com>
2025-05-01 10:35:29 +1200

    Testing renderContent

--- component-library/bookshop/renderContent.js
+++ component-library/bookshop/renderContent.js
@@ -6,7 +6,7 @@ export default function (Liquid) {
     const renderedMarkdownSansShortcode = md.render(value);
 
     const alertShortcodeRegex =
-      /{% bookshop 'snippets\/alert' background_color: "(.*?)" alert_message: "(.*?)" color: "(.*?)" %}/gi;
+      /{% bookshop 'snippets\/alert' background_color: "(.*?)" alert_message: "(.*?)" color: "(.*?)" %}/gim;
 
     const alertShortcodeRendered =
       /<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: $1; color: $3;"><p class="!mb-0"> $2 \<\/p\>\<\/div\>/;
