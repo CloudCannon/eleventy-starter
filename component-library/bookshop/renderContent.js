58cbdb07d3c5344a2b1ddcb8bfba951a5891fdc2
tomrcc <tom@cloudcannon.com>
2025-05-01 10:49:13 +1200

    Testing renderContent

--- component-library/bookshop/renderContent.js
+++ component-library/bookshop/renderContent.js
@@ -3,19 +3,18 @@ const md = new MarkdownIt({ html: true });
 
 export default function (Liquid) {
   this.registerFilter("renderContent", (value) => {
-    const renderedMarkdownSansShortcode = md.render(value);
-
     const alertShortcodeRegex =
-      /<p>{% bookshop 'snippets\/alert' background_color: "(.*?)" alert_message: "(.*?)" color: "(.*?)" %}<\/p>/gim;
+      /{% bookshop 'snippets\/alert' background_color: "(.*?)" alert_message: "(.*?)" color: "(.*?)" %}/gim;
 
     const alertShortcodeRendered =
       /<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: $1; color: $3;"><p class="!mb-0"> $2 \<\/p\>\<\/div\>/;
 
-    const valueWithRenderedShortcode = renderedMarkdownSansShortcode.replaceAll(
+    const valueWithRenderedShortcode = value.replaceAll(
       alertShortcodeRegex,
       alertShortcodeRendered
     );
 
-    return valueWithRenderedShortcode;
+    const renderedMarkdownWithShortcode = md.render(valueWithRenderedShortcode);
+    return renderedMarkdownWithShortcode;
   });
 }
