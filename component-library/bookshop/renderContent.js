6aa6d8c5e41d25961f818c2df1d68baeb91e7617
tomrcc <tom@cloudcannon.com>
2025-05-01 10:14:38 +1200

    Testing renderContent

--- component-library/bookshop/renderContent.js
+++ component-library/bookshop/renderContent.js
@@ -10,11 +10,11 @@ export default function (Liquid) {
 
     console.log(alertShortcodeRegex);
 
-    const alertShortcodeRendered = `
-<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: ${alertShortcodeRegex[$1]}; color: ${alertShortcodeRegex[3]};">
-  <p class="!mb-0">${alertShortcodeRegex[2]}</p>
-</div>
-`;
+    //     const alertShortcodeRendered = `
+    // <div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: ${alertShortcodeRegex[1]}; color: ${alertShortcodeRegex[3]};">
+    //   <p class="!mb-0">${alertShortcodeRegex[2]}</p>
+    // </div>
+    // `;
 
     const testReplacement = "<h1>A snippet should go here</h1>";
 
