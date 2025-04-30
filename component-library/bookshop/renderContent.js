4e6d14d98a5b0bd398d2605702e3c7b870fdbeed
tomrcc <tom@cloudcannon.com>
2025-05-01 10:04:16 +1200

    Test unindenting rendered shortcode

--- component-library/bookshop/renderContent.js
+++ component-library/bookshop/renderContent.js
@@ -6,17 +6,21 @@ export default function (Liquid) {
     const renderedMarkdownSansShortcode = md.render(value);
 
     const alertShortcodeRegex =
-      /{% bookshop 'snippets\/alert' background_color: "(#FF785A)" alert_message: "(Test test test)" color: ("#FEF9EF)" %}/gi;
+      /{% bookshop 'snippets\/alert' background_color: (.*?) alert_message: (.*?) color: (.*?) %}/gi;
+
+    console.log(alertShortcodeRegex);
 
     const alertShortcodeRendered = `
-<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: ${alertShortcodeRegex[1]}; color: ${alertShortcodeRegex[3]};">
+<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: ${alertShortcodeRegex[$1]}; color: ${alertShortcodeRegex[3]};">
   <p class="!mb-0">${alertShortcodeRegex[2]}</p>
 </div>
 `;
 
+    const testReplacement = "<h1>A snippet should go here</h1>";
+
     const valueWithRenderedShortcode = renderedMarkdownSansShortcode.replaceAll(
       alertShortcodeRegex,
-      alertShortcodeRendered
+      testReplacement
     );
 
     return valueWithRenderedShortcode;
