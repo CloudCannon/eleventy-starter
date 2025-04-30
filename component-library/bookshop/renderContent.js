848e0e5f863e4e6337fc81cbd46970a79f3b8b36
tomrcc <tom@cloudcannon.com>
2025-05-01 09:49:54 +1200

    Test unindenting rendered shortcode

--- component-library/bookshop/renderContent.js
+++ component-library/bookshop/renderContent.js
@@ -3,22 +3,22 @@ const md = new MarkdownIt({ html: true });
 
 export default function (Liquid) {
   this.registerFilter("renderContent", (value) => {
+    const renderedMarkdownSansShortcode = md.render(value);
+
     const alertShortcodeRegex =
       /{% bookshop 'snippets\/alert' background_color: "(#FF785A)" alert_message: "(Test test test)" color: ("#FEF9EF)" %}/gi;
 
     const alertShortcodeRendered = `
-    <div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: ${alertShortcodeRegex[1]}; color: ${alertShortcodeRegex[3]};">
-      <p class="!mb-0">${alertShortcodeRegex[2]}</p>
-    </div>
-  `;
+<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: ${alertShortcodeRegex[1]}; color: ${alertShortcodeRegex[3]};">
+  <p class="!mb-0">${alertShortcodeRegex[2]}</p>
+</div>
+`;
 
-    const valueWithRenderedShortcode = value.replaceAll(
+    const valueWithRenderedShortcode = renderedMarkdownSansShortcode.replaceAll(
       alertShortcodeRegex,
       alertShortcodeRendered
     );
 
-    const renderedMarkdownAndShortcode = md.render(valueWithRenderedShortcode);
-
-    return renderedMarkdownAndShortcode;
+    return valueWithRenderedShortcode;
   });
 }
