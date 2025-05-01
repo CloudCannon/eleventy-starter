8fa2e3c7693cc5ab5a236d728f087ec4d5946994
tomrcc <tom@cloudcannon.com>
2025-05-01 13:24:53 +1200

    Test more than one shortcode using on-named groups

--- component-library/bookshop/renderContent.js
+++ component-library/bookshop/renderContent.js
@@ -4,31 +4,29 @@ const md = new MarkdownIt({ html: true });
 export default function (Liquid) {
   this.registerFilter("renderContent", (value) => {
     const fileShortcodeRegex =
-      /{% bookshop 'snippets\/file' src: "(?<filesrc>.*?)" file_name: "(?<filefile_name>.*?)" link_message: "(?<filelink_message>.*?)" %}/gim;
-    const fileShortcodeRendered =
-      '<a href="$filesrc" download="$filefile_name">$filelink_message</a>';
+      /{% bookshop 'snippets\/file' src: "(.*?)" file_name: "(.*?)" link_message: "(.*?)" %}/gim;
+    const fileShortcodeRendered = '<a href="$1" download="$2">$3</a>';
 
     const valueWithRenderedFileShortcode = value.replaceAll(
       fileShortcodeRegex,
       fileShortcodeRendered
     );
 
-    const matches = value.matchAll(fileShortcodeRegex);
-    console.log(matches);
-    console.log(matches.groups);
+    // const matches = value.matchAll(fileShortcodeRegex);
+    // console.log(matches);
 
-    for (const match of matches) {
-      const captures = [...match];
-      for (let i = 0; i < captures.length; i++) {
-        console.log(`[${i}] ${captures[i]}`);
-      }
-    }
+    // for (const match of matches) {
+    //   const captures = [...match];
+    //   for (let i = 0; i < captures.length; i++) {
+    //     console.log(`[${i}] ${captures[i]}`);
+    //   }
+    // }
 
     const alertShortcodeRegex =
-      /{% bookshop 'snippets\/alert' background_color: "(?<alertbackground_color>.*?)" alert_message: "(?<alertalert_message>.*?)" color: "(?<alert_color>.*?)" %}/gim;
+      /{% bookshop 'snippets\/alert' background_color: "(.*?)" alert_message: "(.*?)" color: "(.*?)" %}/gim;
 
     const alertShortcodeRendered =
-      '<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: $alertbackground_color; color: $alert_color;"><p class="!mb-0"> $alertalert_message </p></div>';
+      '<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: $1; color: $3;"><p class="!mb-0"> $2 </p></div>';
 
     const valueWithRenderedAlertShortcode =
       valueWithRenderedFileShortcode.replaceAll(
