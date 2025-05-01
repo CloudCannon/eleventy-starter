3826d5080b7e6734cd18e295dc17e13154bf1a16
tomrcc <tom@cloudcannon.com>
2025-05-01 14:01:33 +1200

    Test adding video snippet

--- component-library/bookshop/renderContent.js
+++ component-library/bookshop/renderContent.js
@@ -1,7 +1,7 @@
 import MarkdownIt from "markdown-it";
 const md = new MarkdownIt({ html: true });
 
-const snippetData = {
+const allSnippets = {
   file: {
     regex:
       /{% bookshop 'snippets\/file' src: "(.*?)" file_name: "(.*?)" link_message: "(.*?)" %}/gim,
@@ -13,42 +13,26 @@ const snippetData = {
     rendered:
       '<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: $1; color: $3;"><p class="!mb-0"> $2 </p></div>',
   },
+  video: {
+    regex: /{% bookshop 'snippets\/video' src: "(.*?)" %}/gim,
+    rendered:
+      '<video class="py-6" controls><source src=$1 type="video/mp4" /> Your browser does not support the video tag.</video>',
+  },
 };
-
-// const fileShortcodeRegex =
-//   /{% bookshop 'snippets\/file' src: "(.*?)" file_name: "(.*?)" link_message: "(.*?)" %}/gim;
-// const fileShortcodeRendered = '<a href="$1" download="$2">$3</a>';
-
-// const alertShortcodeRegex =
-//   /{% bookshop 'snippets\/alert' background_color: "(.*?)" alert_message: "(.*?)" color: "(.*?)" %}/gim;
-// const alertShortcodeRendered =
-//   '<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: $1; color: $3;"><p class="!mb-0"> $2 </p></div>';
-
-const allSnippets = Object.keys(snippetData);
+const allSnippetKeys = Object.keys(allSnippets);
 
 export default function (Liquid) {
   this.registerFilter("renderContent", (value) => {
     let markdownToWrite = value;
 
-    allSnippets.map((snippet) => {
-      const snippetInfo = snippetData[snippet];
+    allSnippetKeys.map((snippet) => {
+      const snippetData = allSnippets[snippet];
 
       markdownToWrite = markdownToWrite.replaceAll(
-        snippetInfo.regex,
-        snippetInfo.rendered
+        snippetData.regex,
+        snippetData.rendered
       );
     });
-
-    // const valueWithRenderedFileShortcode = value.replaceAll(
-    //   fileShortcodeRegex,
-    //   fileShortcodeRendered
-    // );
-
-    // const valueWithRenderedAlertShortcode =
-    //   valueWithRenderedFileShortcode.replaceAll(
-    //     alertShortcodeRegex,
-    //     alertShortcodeRendered
-    //   );
 
     const renderedMarkdownWithShortcodes = md.render(markdownToWrite);
     return renderedMarkdownWithShortcodes;
