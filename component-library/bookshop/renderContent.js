041a719b232294911aefb82d81b24c6eb1bce77b
tomrcc <tom@cloudcannon.com>
2025-05-01 13:56:57 +1200

    Test the same logic in a loop

--- component-library/bookshop/renderContent.js
+++ component-library/bookshop/renderContent.js
@@ -1,32 +1,56 @@
 import MarkdownIt from "markdown-it";
 const md = new MarkdownIt({ html: true });
 
+const snippetData = {
+  file: {
+    regex:
+      /{% bookshop 'snippets\/file' src: "(.*?)" file_name: "(.*?)" link_message: "(.*?)" %}/gim,
+    rendered: '<a href="$1" download="$2">$3</a>',
+  },
+  alert: {
+    regex:
+      /{% bookshop 'snippets\/alert' background_color: "(.*?)" alert_message: "(.*?)" color: "(.*?)" %}/gim,
+    rendered:
+      '<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: $1; color: $3;"><p class="!mb-0"> $2 </p></div>',
+  },
+};
+
+// const fileShortcodeRegex =
+//   /{% bookshop 'snippets\/file' src: "(.*?)" file_name: "(.*?)" link_message: "(.*?)" %}/gim;
+// const fileShortcodeRendered = '<a href="$1" download="$2">$3</a>';
+
+// const alertShortcodeRegex =
+//   /{% bookshop 'snippets\/alert' background_color: "(.*?)" alert_message: "(.*?)" color: "(.*?)" %}/gim;
+// const alertShortcodeRendered =
+//   '<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: $1; color: $3;"><p class="!mb-0"> $2 </p></div>';
+
+const allSnippets = Object.keys(snippetData);
+
 export default function (Liquid) {
   this.registerFilter("renderContent", (value) => {
-    const fileShortcodeRegex =
-      /{% bookshop 'snippets\/file' src: "(.*?)" file_name: "(.*?)" link_message: "(.*?)" %}/gim;
-    const fileShortcodeRendered = '<a href="$1" download="$2">$3</a>';
+    let markdownToWrite = value;
 
-    const valueWithRenderedFileShortcode = value.replaceAll(
-      fileShortcodeRegex,
-      fileShortcodeRendered
-    );
+    allSnippets.map((snippet) => {
+      const snippetInfo = snippetData[snippet];
 
-    const alertShortcodeRegex =
-      /{% bookshop 'snippets\/alert' background_color: "(.*?)" alert_message: "(.*?)" color: "(.*?)" %}/gim;
-
-    const alertShortcodeRendered =
-      '<div class="flex items-center gap-4 px-4 py-2 rounded-lg shadow-md my-4" style="background-color: $1; color: $3;"><p class="!mb-0"> $2 </p></div>';
-
-    const valueWithRenderedAlertShortcode =
-      valueWithRenderedFileShortcode.replaceAll(
-        alertShortcodeRegex,
-        alertShortcodeRendered
+      markdownToWrite = markdownToWrite.replaceAll(
+        snippetInfo.regex,
+        snippetInfo.rendered
       );
+    });
 
-    const renderedMarkdownWithShortcode = md.render(
-      valueWithRenderedAlertShortcode
-    );
-    return renderedMarkdownWithShortcode;
+    // const valueWithRenderedFileShortcode = value.replaceAll(
+    //   fileShortcodeRegex,
+    //   fileShortcodeRendered
+    // );
+
+    // const valueWithRenderedAlertShortcode =
+    //   valueWithRenderedFileShortcode.replaceAll(
+    //     alertShortcodeRegex,
+    //     alertShortcodeRendered
+    //   );
+
+    const renderedMarkdownWithShortcodes = md.render(markdownToWrite);
+    return renderedMarkdownWithShortcodes;
   });
 }
